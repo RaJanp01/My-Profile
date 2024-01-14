44c1776e51ce495643d425cd851e08766ea4737f
RaJanp01 <rajanp860@gmail.com>
2024-01-14 07:15:52 -0500

    design changes

--- script/script.js
+++ script/script.js
@@ -1,4 +1,24 @@
+document.addEventListener("DOMContentLoaded", function() {
+    window.addEventListener('scroll', revealCardsOnScroll);
+    revealCardsOnScroll(); // Run on initial load to check for cards already in view
+});
 
-function uselessButton(){
-    alert("I love you babe❤️");
+function revealCardsOnScroll() {
+    var cards = document.querySelectorAll('.card');
+
+    cards.forEach((card) => {
+        if (isElementInViewport(card)) {
+            card.classList.add('show');
+        } else {
+            card.classList.remove('show');
+        }
+    });
+}
+
+function isElementInViewport(el) {
+    var rect = el.getBoundingClientRect();
+    return (
+        rect.top <= (window.innerHeight || document.documentElement.clientHeight) &&
+        rect.bottom >= 0 
+    );
 }
